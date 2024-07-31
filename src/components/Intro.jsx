ab470b91f20176adaedd5f1e62f04784d2874cc8
Jueuuun <cute1845f@gmail.com>
2024-07-31 11:48:29 +0900

    03_Skill 완료
    03_Skill 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 
+// 이미지 링크 걸어야함
+import about from "../assets/img/about.png";
+
 const Intro = () => {
   return (
-    <div>
-      Intro
-    </div>
+    <section id='intro'>
+      <div className="intro_inner">
+        <h1 className='intro_title'>
+          HAN JU EUN 
+        </h1>
+        <div className="intro_lines" aria-hidden="true">
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+        </div>
+
+        <div className="intro_text">
+          <div className="text">
+            <div>MOON</div>
+            <div>NIGHT</div>
+            <div>STAR</div>
+          </div>
+          <div className="img">
+            <img src={about} alt="이미지 연결" />
+          </div>
+        </div>
+
+        <div className="intro_lines bottom" aria-hidden="true">
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+        </div>
+      </div>
+    </section>
   )
 }
 
